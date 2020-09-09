ad17497bb451ef2d4ab0e11713f8431c3b2fdee5
Guilherme Nathaniel <60532293+guiro00@users.noreply.github.com>
2020-09-09 00:09:56 -0400

    Add files via upload

---
+++ index.php
@@ -0,0 +1,167 @@
+<!DOCTYPE HTML>
+<!--
+	Portfólio Guilherme
+-->
+<html>
+
+<head>
+	<title>Valentine Fotográfias</title>
+	<meta charset="utf-8" />
+	<meta name="viewport" content="width=device-width, initial-scale=1" />
+	<link rel="stylesheet" href="assets/css/main.css" />
+</head>
+
+<body>
+	<div class="page-wrap">
+
+		<!-- Nav -->
+		<nav id="nav">
+			<ul>
+				<li><a href="index.html" class="active"><span class="icon fa-home"></span></a></li>
+				<li><a href="gallery.html"><span class="icon fa-camera-retro"></span></a></li>
+
+			</ul>
+		</nav>
+
+		<!-- Main -->
+		<section id="main">
+
+			<!-- Banner -->
+			<section id="banner">
+				<div class="inner">
+					<h1>Ei, bem vindo a minha Galeria</h1>
+					<p>Conheça um pouco mais das minhas <a href="https://templated.co">FOTOGRÁFIAS</a></p>
+					<ul class="actions">
+						<li><a href="#galleries" class="button alt scrolly big">Continue</a></li>
+					</ul>
+				</div>
+			</section>
+
+			<!-- Gallery -->
+			<section id="galleries">
+
+				<!-- Photo Galleries -->
+				<div class="gallery">
+					<header class="special">
+						<h2>O que há de novo</h2>
+					</header>
+					<div class="content">
+						<div class="media">
+							<a href="images/fulls/15.jpg"><img src="images/thumbs/15.jpg" alt=""
+									title="This right here is a caption." /></a>
+						</div>
+						<div class="media">
+							<a href="images/fulls/16.jpg"><img src="images/thumbs/16.jpg" alt=""
+									title="This right here is a caption." /></a>
+						</div>
+						<div class="media">
+							<a href="images/fulls/17.jpg"><img src="images/thumbs/17.jpg" alt=""
+									title="This right here is a caption." /></a>
+						</div>
+						<div class="media">
+							<a href="images/fulls/18.jpg"><img src="images/thumbs/18.jpg" alt=""
+									title="This right here is a caption." /></a>
+						</div>
+						<div class="media">
+							<a href="images/fulls/19.jpg"><img src="images/thumbs/19.jpg" alt=""
+									title="This right here is a caption." /></a>
+						</div>
+						<div class="media">
+							<a href="images/fulls/20.jpg"><img src="images/thumbs/20.jpg" alt=""
+									title="This right here is a caption." /></a>
+						</div>
+						<div class="media">
+							<a href="images/fulls/21.jpg"><img src="images/thumbs/21.jpg" alt=""
+									title="This right here is a caption." /></a>
+						</div>
+						<div class="media">
+							<a href="images/fulls/03.jpg"><img src="images/thumbs/03.jpg" alt=""
+									title="This right here is a caption." /></a>
+						</div>
+					</div>
+					<footer>
+						<a href="gallery.html" class="button big">Galeria</a>
+					</footer>
+				</div>
+			</section>
+			<section class="columns double">
+				<div class="column">
+					<span class="image left special"><img src="images/22.jpg" alt="" /></span>
+					<h3>Sobre mim</h3>
+					<p>
+						Olá o meu nome é Guilherme e sou Fotógrafo e designer de fotográfias, pois atualmente eu mexo
+						com
+						fotográfias e web designer de páginas e programação.
+					</p>
+			</section>
+
+			<!-- Contact -->
+			<section id="contact">
+				<!-- Social -->
+				<div class="social column">
+					<h3><strong>Um pouco mais sobre mim</strong></h3>
+					<p>Vou contar um pouco mais sobre mim.<br>Eu nasci na cidade de corumbá que fica localizado no
+						estado de Mato Grosso do Sul, tenho 23 anos e estou cursando Sistemas de informações na UFMS,
+						além desse Hobby de fotográfias eu tambem sou dançarino e
+						coreógrafo da minha cidade, pois além de mim existe vários outros artistas de várias modalidade
+						que são cheios de talentos e tem vontade de mostrar isso ao mundo. Atualmente eu faço parte do
+						grupo URBE ( grupo fundado pelo dançarino, coreógrafo e interprete, Edelton Mendes Amorim) um
+						grupo que tem o intuito de passar conhecimento sobre estilos urbanos e agregar com a
+						cultura.<br>Se quiser saber mais sobre mim é só me acompanhar nas redes sociais.<br>Um forte
+						abraço a todos.
+					</p>
+					<h3>Siga-me</h3>
+					<ul class="icons">
+						<li><a href="#" class="icon fa-twitter"><span class="label">Twitter</span></a></li>
+						<li><a href="https://www.facebook.com/guilherme.nathaniel.35/" class="icon fa-facebook"><span
+									class="label">Facebook</span></a></li>
+						<li><a href="https://www.instagram.com/guilherme.nathaniel/?hl=pt-br" class="icon fa-instagram"><span
+									class="label">Instagram</span></a></li>
+					</ul>
+				</div>
+
+				<!-- Form -->
+				<div class="column">
+					<h3>Entra em contato</h3>
+					<form action="#" method="post">
+						<div class="field half first">
+							<label for="name">Nome</label>
+							<input name="name" id="name" type="text" placeholder="Nome">
+						</div>
+						<div class="field half">
+							<label for="email">Email</label>
+							<input name="email" id="email" type="email" placeholder="Email">
+						</div>
+						<div class="field">
+							<label for="message">Mensagem</label>
+							<textarea name="message" id="message" rows="6" placeholder="Mensagem"></textarea>
+						</div>
+						<ul class="actions">
+							<li><input value="Enviar Mensagem" class="button" type="submit"></li>
+						</ul>
+					</form>
+				</div>
+
+			</section>
+
+			<!-- Footer -->
+			<footer id="footer">
+				<div class="copyright">
+					&copy; Web Designer: <a href="https://www.facebook.com/guilherme.nathaniel.35/">Guilherme
+						Valentine</a>. Images: <a href="https://unsplash.com/">Unsplash</a>.
+				</div>
+			</footer>
+		</section>
+	</div>
+
+	<!-- Scripts -->
+	<script src="assets/js/jquery.min.js"></script>
+	<script src="assets/js/jquery.poptrox.min.js"></script>
+	<script src="assets/js/jquery.scrolly.min.js"></script>
+	<script src="assets/js/skel.min.js"></script>
+	<script src="assets/js/util.js"></script>
+	<script src="assets/js/main.js"></script>
+
+</body>
+
+</html>
