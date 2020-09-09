7bc9d939de238a644c7652a2d9410dbd16d399ba
guiro00 <guilhermecampos20188@gmail.com>
2020-09-09 00:41:57 -0500

    partes

--- index.php
+++ index.php
@@ -120,27 +120,7 @@
 					</ul>
 				</div>
 
-				<!-- Form -->
-				<div class="column">
-					<h3>Entra em contato</h3>
-					<form action="#" method="post">
-						<div class="field half first">
-							<label for="name">Nome</label>
-							<input name="name" id="name" type="text" placeholder="Nome">
-						</div>
-						<div class="field half">
-							<label for="email">Email</label>
-							<input name="email" id="email" type="email" placeholder="Email">
-						</div>
-						<div class="field">
-							<label for="message">Mensagem</label>
-							<textarea name="message" id="message" rows="6" placeholder="Mensagem"></textarea>
-						</div>
-						<ul class="actions">
-							<li><input value="Enviar Mensagem" class="button" type="submit"></li>
-						</ul>
-					</form>
-				</div>
+			
 
 			</section>
 
