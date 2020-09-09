ed6293af7baf6fc341b098c5f00fdbc9cb650f23
guiro00 <guilhermecampos20188@gmail.com>
2020-09-09 01:06:20 -0500

    partes

--- index.php
+++ index.php
@@ -141,6 +141,7 @@
 	<script src="assets/js/skel.min.js"></script>
 	<script src="assets/js/util.js"></script>
 	<script src="assets/js/main.js"></script>
+	<script src="gallery.js"></script>
 
 </body>
 
